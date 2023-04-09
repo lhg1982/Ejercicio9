4233c0fca137060cc74a4b72d8ad328aa10ef4ae
lhg1982 <leonardohernan.gomez@outlook.com>
2023-04-09 09:21:29 -0300

    Ejercicio Personas

---
+++ src/Padron.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Padron {
+
+    private HashMap<Integer, Persona> personas;
+
+    public Padron(){
+        personas = new HashMap<>();
+        }
+
+    public void agregarPersona(Persona nueva){
+        //Podria validar.
+        personas.put(nueva.getDocumento(), nueva);
+    }
+
+     public float promedioEdades(){
+        int suma = 0;
+         for (Persona p : personas.values()){
+             suma += p.getEdad();
+         }
+         return (float)suma / personas.size();
+     }
+     public Persona buscarPersona(int buscado){
+        return personas.get(buscado);
+     }
+     public ArrayList<Persona> buscarPorNombre(String buscado){
+        ArrayList<Persona> encontradas = new ArrayList<>();
+
+        for(Persona p: personas.values()){
+            if (p.getNombre().equals(buscado)){
+                encontradas.add(p);
+            }
+        }
+        return encontradas;
+     }
+
+}
